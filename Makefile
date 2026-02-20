0baed79a0e64b58511fb33829abad72f4a7577b5
nati <nati@debian.nati>
2026-02-20 22:20:41 +0100

    add make, finish wordpress setup

---
+++ Makefile
@@ -0,0 +1,18 @@
+all:
+	docker compose -f srcs/docker-compose.yml up --build
+
+down:
+	docker compose -f srcs/docker-compose.yml down
+
+re:
+	docker compose -f srcs/docker-compose.yml down -v
+	docker compose -f srcs/docker-compose.yml up --build
+
+clean:
+	docker compose -f srcs/docker-compose.yml down -v
+	docker system prune -af
+
+fclean: clean
+	docker volume prune -f
+
+.PHONY: all down re clean fclean
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+mkdir -p /var/www/html
+cd /var/www/html
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    wp core download --allow-root
+
+    wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb
+
+    wp core install --allow-root \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
+
+    wp user create --allow-root \
+        $WP_USER $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PASSWORD
+fi
+
+exec php-fpm8.2 -F
