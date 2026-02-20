c1d3b71cb6ebcb48428bbc9b4b5a9b576c8ad8c9
nati <nati@debian.nati>
2026-02-20 19:09:42 +0100

    mariadb working

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+mysqld_safe --skip-networking &
+sleep 3
+
+mysql -u root << EOF
+
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
