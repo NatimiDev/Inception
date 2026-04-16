503afd279312310776c82a9110de0797589ccda1
Nati Mi <nati.mikula@gmail.com>
2026-04-16 18:19:04 +0200

    Update setup.sh

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,10 +3,12 @@
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 mysqld_safe --skip-networking &
-sleep 3
+
+until mysqladmin ping --silent; do
+    sleep 1
+done
 
 mysql -u root << EOF
-
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
